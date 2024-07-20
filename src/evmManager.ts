4ff7225751aa9a61234e3216660b195f562e4c01
ricardocr987 <ricardocr987@gmail.com>
2024-07-20 12:36:03 +0200

    update AVAX USDC CA

--- src/evmManager.ts
+++ src/evmManager.ts
@@ -65,7 +65,7 @@ export const evmManager = new Elysia({ prefix: '/evm' })
             gas: "0",
             memo: "",
             format: "json",
-            tokenId: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+            tokenId: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
             validatorAddress: "",
             params: {
               pubKey: query.signer
